5a6fbfd9bd7c6e05a25627df90b873301e3fe509
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2022-11-18 12:32:26 +0000

    Testing Hohmann transfer

--- src/backend/main.cpp
+++ src/backend/main.cpp
@@ -6,11 +6,27 @@
 
 int main()
 {
-    SSTP::Body body {SSTP::Vector{{2, 1}}, 2, 0.7};
+    SSTP::Body body1 {SSTP::Vector{{2, 1}}, 2, 0};
+    // SSTP::Body body2 {SSTP::Vector{{2, 1}}, 0.1, 0};
 
-    constexpr int N = 1000;
+    constexpr int N = 500;
+    int stage = 0;
     for (int i = 0; i < N; i++) {
-        body.set_angle(2 * i * M_PI / N);
-        std::cout << body.position() << std::endl;
+        body1.set_angle(4 * i * M_PI / N);
+        if (stage == 0 && body1.angle() >= M_PI) {
+            stage = 1;
+            double semi_major_axis = body1.semi_major_axis() + 1;
+            double eccentricity = 1 - body1.periapsis() / semi_major_axis;
+            body1.update_orbit(semi_major_axis, eccentricity);
+        }
+        if (stage == 1 && body1.angle() >= M_PI * 2) {
+            stage = 2;
+            double semi_major_axis = body1.apsis();
+            double eccentricity = body1.apsis() / semi_major_axis - 1;
+            body1.update_orbit(semi_major_axis, eccentricity);
+        }
+        std::cout << body1.position() << std::endl;
+        // body2.set_angle(60 * i * M_PI / N);
+        // std::cout << body1.position() + body2.position() << std::endl;
     }
 }
