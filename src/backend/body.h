94bfc10c40187404045ef532b6b144acdbefdebb
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2022-11-18 12:31:48 +0000

    Added a way to change orbit properties

--- src/backend/body.h
+++ src/backend/body.h
@@ -23,20 +23,23 @@ namespace SSTP {
 
         Vector position() const;
         OrbitType orbit_type() const;
+        const Vector& direction() const;
         double semi_major_axis() const;
         double semi_minor_axis() const;
+        double apsis() const;
+        double periapsis() const;
         double focal_distance() const;
-        double excentricity() const;
+        double eccentricity() const;
         double eccentric_anomaly() const;
         double angle() const;
         double radius() const;
         void set_angle(double angle);
+        void update_orbit(const Vector& direction, double semi_major_axis, double excentricity);
+        void update_orbit(double semi_major_axis, double excentricity);
 
     private:
         double a, b, c, e, theta, i;
-        Vector direction;
-
-        void calculate_parameters(const Vector& direction, double semi_major_axis, double excentricity);
+        Vector d;
     };
 }
 
